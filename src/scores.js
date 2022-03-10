c72d9c40152fe7724ee1af4051ae38b566673f9a
unknown <okorojiebube2@gmail.com>
2022-03-09 21:50:16 -0600

    Add render score functionality

--- src/scores.js
+++ src/scores.js
@@ -1,16 +1,15 @@
 import { getData } from './services'
 
  const renderScore = async scores => {
-	document.querySelector('.leaderboard').innerHTML = scores
-		.map(({ user, score }) => {
-			return `
+ scores.forEach(({ user, score }) => {
+
+ document.querySelector('.leaderboard').innerHTML = `
    <li>${user}  ${score}</li>
    `
 		})
-		.join('')
 }
 
-
+	
 export const refreshData = async () => {
   let userDatas = await getData()
 	userDatas.sort((playerOne, playerTwo) => playerTwo.score - playerOne.score)
